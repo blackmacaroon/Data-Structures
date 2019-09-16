e8afde4cb0dd274fdae3af54a71538c74fe1da10
Kayla <6lackmacaroon@gmail.com>
2019-09-16 16:48:10 -0600

    stack push and pop filled out, no tests so I'm unsure if they pass

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,15 +6,15 @@ class Stack:
   def __init__(self):
     self.size = 0
     # Why is our DLL a good choice to store our elements?
-    self.storage = DoublyLinkedList
+    self.storage = DoublyLinkedList()
 
   def push(self, value):
     self.storage.add_to_head(value)
     self.size += 1
   
   def pop(self):
-    self.storage.remove_from_head()
     self.size -= 1
+    return self.storage.remove_from_head()
 
   def len(self):
     return self.size
