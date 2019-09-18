60557d5b3a61be89c4563102335b190da821f8d8
Kayla <6lackmacaroon@gmail.com>
2019-09-18 14:04:02 -0600

    three fails and one error, yessss

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -19,21 +19,38 @@ class BinarySearchTree:
   def insert(self, value):
     #if greater than root, move right
     #if less than root, move left
+    #recursion?
     pass
   '''search the tree for input value, return boolean whether it exists or not
   start from root and traverse the tree
   stop at the first instance of a value
   its not found it we get to a node that doesn't have children'''
   def contains(self, target):
+    #edge cases, no kiddos
+    if not self.right and not self.left:
+      return False
     #if target = node value, return true
-    #else if target > node, go right
-    #else if target < node, go left
-    pass
+    elif target is self.value:
+      return True
+    #else if target > node, go right, recursively calling contains
+    elif target > self.value:
+      return self.right.contains(target)
+    #else if target < node, go left, recursively calling contains
+    elif target < self.value:
+      return self.left.contains(target)
+    else:
+      print("heidy-ho neighbor")
+      #what else?
   '''returns the max value in the tree
   for each, count += 1'''
   def get_max(self):
     #hard right til no child = largest 
-    pass
+    if self.right:
+      return self.right.get_max()
+    #edge case, you're the largest!
+    else:
+      return self.value
+    
   '''traverses every node in the tree, executing the passed-in callback function on each tree node value. '''
   def for_each(self, cb):
     #recursion ?
