ca1575f97eb7d7f310b2a2c02a286051852219ff
Kayla <6lackmacaroon@gmail.com>
2019-09-18 13:37:11 -0600

    understanding the problem, pseudocoding

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,17 +17,24 @@ class BinarySearchTree:
     self.right = None
   '''adds the input value to the binary search tree, adhering to tree order'''
   def insert(self, value):
+    #if greater than root, move right
+    #if less than root, move left
     pass
   '''search the tree for input value, return boolean whether it exists or not
   start from root and traverse the tree
   stop at the first instance of a value
   its not found it we get to a node that doesn't have children'''
   def contains(self, target):
+    #if target = node value, return true
+    #else if target > node, go right
+    #else if target < node, go left
     pass
   '''returns the max value in the tree
   for each, count += 1'''
   def get_max(self):
+    #hard right til no child = largest 
     pass
   '''traverses every node in the tree, executing the passed-in callback function on each tree node value. '''
   def for_each(self, cb):
+    #recursion ?
     pass
